2b4e5827f4cc98a77ec6092b99c08e8f50b0e031
UtkarshaJoshi <joshiutkarsha3@gmail.com>
2017-10-03 15:28:50 +0530

    i have added a sop in the file

--- app/src/main/java/com/example/alpha/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/alpha/githubdemo/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
             System.out.println("## 123");
+        System.out.println("##HELLO KIRAN SIRRR");
+        System.out.println("##HELLO KIRAN SIRRR");
+
+        System.out.println("##HELLO KIRAN SIRRR");
+
 
 
 
