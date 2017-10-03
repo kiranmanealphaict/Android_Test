d6a27fe51981160d774c4b65598934a285f28cf9
alpha <kiranmanealphaict@gmail.com>
2017-10-03 15:35:04 +0530

    add hello by kiran

--- app/src/main/java/com/example/alpha/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/alpha/githubdemo/MainActivity.java
@@ -9,16 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("## 123");
             System.out.println("## 123");
         System.out.println("##HELLO KIRAN SIRRR");
         System.out.println("##HELLO KIRAN SIRRR");
 
         System.out.println("##HELLO KIRAN SIRRR");
-
-
-
-        System.out.println("## hello");
 
 
     }
